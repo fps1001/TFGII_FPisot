4d3c2e1ef11195a07b4849f1f91a4e1e09841765
F. Pisot <fps1001@alu.ubu.es>
2024-07-29 13:21:44 +0200

    #7 #8 Unificación bloc. Añadidos permisos a manifest

--- project-app/project_app/lib/blocs/gps/gps_bloc.dart
+++ project-app/project_app/lib/blocs/gps/gps_bloc.dart
@@ -5,9 +5,13 @@ part 'gps_event.dart';
 part 'gps_state.dart';
 
 class GpsBloc extends Bloc<GpsEvent, GpsState> {
-  GpsBloc() : super(const GpsState(isGpsEnabled: false, isGpsPermissionGranted: false)); {
-    on<GpsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  GpsBloc() : super(const GpsState(isGpsEnabled: false, isGpsPermissionGranted: false)) {
+    
+    on<OnGpsAndPermissionEvent>((event, emit)  // Se dispara al recibir un evento de tipo OnGpsAndPermissionEvent
+      => emit(state.copyWith( 
+        isGpsEnabled: event.isGpsEnabled, // Cambia el estado del GPS
+        isGpsPermissionGranted: event.isGpsPermissionGranted, // Cambia el estado de los permisos
+      ))
+    );
   }
 }
--- project-app/project_app/lib/blocs/gps/gps_event.dart
+++ project-app/project_app/lib/blocs/gps/gps_event.dart
@@ -6,3 +6,13 @@ sealed class GpsEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class OnGpsAndPermissionEvent extends GpsEvent { // Evento que se dispara cuando se comprueba si el GPS está activado y si la aplicación tiene permisos para acceder a él
+  final bool isGpsEnabled;
+  final bool isGpsPermissionGranted;
+
+  const OnGpsAndPermissionEvent({
+    required this.isGpsEnabled,
+    required this.isGpsPermissionGranted,
+  });
+}
