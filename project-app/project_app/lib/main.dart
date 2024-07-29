29173ee85fd5e6a03eae2e64cefed5a4ad53f607
F. Pisot <fps1001@alu.ubu.es>
2024-07-29 10:12:14 +0200

    Proyecto Flutter creado.

---
+++ project-app/project_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:project_app/screens/screens.dart';
+
+void main() => runApp(const ProjectApp());
+
+class ProjectApp extends StatelessWidget {
+  const ProjectApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'EcoTour App',
+        home: LoadingScreen()
+    );
+  }
+}
---
+++ project-app/project_app/lib/screens/screens.dart
@@ -0,0 +1,4 @@
+
+export 'package:project_app/screens/gps_access_screen.dart';
+export 'package:project_app/screens/loading_screen.dart';
+export 'package:project_app/screens/map_screen.dart';
