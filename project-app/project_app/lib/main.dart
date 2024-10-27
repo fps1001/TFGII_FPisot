09e3cff87c2d48193ecdef0e02bfe194b8b39a27
Fernando Pisot Serrano <fps1001@alu.ubu.es>
2024-10-27 11:37:45 +0100

    #60 Se modifica la declaración de firebase para usar .env. Ejecutando correctamente.

--- project-app/project_app/lib/main.dart
+++ project-app/project_app/lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-import 'firebase_options.dart';
 import 'package:project_app/blocs/blocs.dart';
 import 'package:project_app/router/app_router.dart';
 import 'package:project_app/services/services.dart';
@@ -12,44 +10,48 @@ import 'package:project_app/themes/themes.dart';
 import 'package:project_app/logger/logger.dart';
 
 void main() async {
-  // Asegurarse de que Flutter se haya inicializado
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializar Firebase según indica la consola.
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // Cargar variables de entorno
+  await dotenv.load(fileName: ".env");
 
-  // Configurar Crashlytics
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Inicializar Firebase usando las variables de entorno
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+    ),
+  );
 
   Bloc.observer = MyBlocObserver();
 
   try {
-    await dotenv.load(fileName: '.env');
     log.i('Variables de entorno cargadas');
   } catch (e) {
     log.e("Error en archivo .env: $e");
-    // También registrar el error en Crashlytics
-    FirebaseCrashlytics.instance.recordError(e, null);
   }
 
-  // Ejecutar la aplicación con MultiBlocProvider
   runApp(MultiBlocProvider(
+    // En vez de hacer runApp se añade un multiblocprovider para gestionar los blocs de la app.
     providers: [
-      BlocProvider(create: (context) => GpsBloc()), // Gestión de permiso de localización y GPS activo.
-      BlocProvider(create: (context) => LocationBloc()), // Gestión de localización de usuario.
       BlocProvider(
-        create: (context) => MapBloc(
-          locationBloc: BlocProvider.of<LocationBloc>(context),
-        ),
-      ), // Gestión del controlador de mapa.
+          create: (context) =>
+              GpsBloc()), // Gestión de permiso de localización y GPS activo.
       BlocProvider(
-        create: (context) => TourBloc(
-          optimizationService: OptimizationService(),
-          mapBloc: BlocProvider.of<MapBloc>(context),
-        ),
-      ), // Información del Tour y sus POIs
+          create: (context) =>
+              LocationBloc()), // Gestión de localización de usuario.
+      BlocProvider(
+          create: (context) => MapBloc(
+              locationBloc: BlocProvider.of<LocationBloc>(
+                  context))), // Gestión del controlador de mapa.
+      BlocProvider(
+          create: (context) => TourBloc(
+              optimizationService: OptimizationService(),
+              mapBloc: BlocProvider.of<MapBloc>(
+                  context))), // Guarda la información del Tour y sus POIs.
     ],
     child: const ProjectApp(),
   ));
@@ -61,10 +63,9 @@ class ProjectApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Eco-City Tour',
-      theme: AppTheme.lightTheme,
-      routerConfig: AppRouter.router,
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Eco-City Tour',
+        theme: AppTheme.lightTheme,
+        routerConfig: AppRouter.router);
   }
 }
