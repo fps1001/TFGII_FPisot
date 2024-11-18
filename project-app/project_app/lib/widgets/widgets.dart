9bc18efc4d9e8d2a29c3e4b92c6d9f48e0ef7cdd
Fernando Pisot Serrano <fps1001@alu.ubu.es>
2024-11-18 21:28:57 +0100

    #63 Añadidos test unitarios de widgets.

--- project-app/project_app/lib/widgets/widgets.dart
+++ project-app/project_app/lib/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'package:project_app/widgets/btn_follow_user.dart';
-export 'package:project_app/widgets/btn_location.dart';
 export 'package:project_app/widgets/btn_toggle_user_route.dart';
 export 'package:project_app/widgets/custom_app_bar.dart';
 export 'package:project_app/widgets/custom_bottom_sheet.dart';
